f804efd7bf59ce8631cf0c9b74d5ea67d36dd6c7
mamon0965 <mamon0965@gmail.com>
2025-11-28 15:49:34 +0900

    feaf 플레이어 이동 구현

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,52 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            //콘솔창 초기화
             Console.ResetColor();
-
             Console.BackgroundColor = ConsoleColor.Magenta;
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.Title = "Sokoban";
             Console.CursorVisible = false;
             Console.Clear();
+
+            //플레이어 초기위치
+            int x = 5;
+            int y = 10;
+            Console.SetCursorPosition(x, y);
+            Console.Write("P");
+
+            //플레이어 이동
+            while (true)
+            {   //키보드 입력
+                ConsoleKeyInfo input = Console.ReadKey();
+                if (input.Key == ConsoleKey.LeftArrow)
+                {
+                    --x;
+                }
+                else if (input.Key == ConsoleKey.UpArrow)
+                {
+                    --y;
+                }
+                else if (input.Key == ConsoleKey.RightArrow)
+                {
+                    ++x;
+                }
+                else if (input.Key == ConsoleKey.DownArrow)
+                {
+                    ++y;
+                }
+                Console.Clear();
+                Console.SetCursorPosition(x, y);
+                Console.Write("P");
+                Thread.Sleep(1);
+
+            }
         }
     }
 }
